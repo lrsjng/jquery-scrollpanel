377f32125d1b979e45c68753250baec89b46be71
Lars Jung <lrsjng@gmail.com>
2012-07-04 17:13:11 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# jQuery.scrollpanel
+
+* Website with download, docs and demo: <http://larsjung.de/scrollpanel>
+* Sources: <http://github.com/lrsjng/jQuery.scrollpanel>
+
+jQuery.scrollpanel is provided under the terms of the [MIT License](http://github.com/lrsjng/jQuery.scrollpanel/blob/master/LICENSE.txt).
+
+To enable mouse wheel support add [jQuery.mousewheel](http://github.com/brandonaaron/jquery-mousewheel) by Brandon Aaron (MIT License).
+
+## Changelog
+
+
+### v0.1 - *2012-07-04*
+
+* Initial release
---
+++ src/demo/main.js
@@ -0,0 +1,53 @@
+
+// @include "../inc/jquery-1.7.js"
+// @include "../inc/jquery.mousewheel-3.0.6.js"
+// @include "../jquery.scrollpanel-%BUILD_VERSION%.js"
+
+(function($) {
+	'use strict';
+
+	var generateContent = function () {
+
+			$('.scrollpanel').each(function () {
+
+				var $this = $(this);
+
+				for (var i = 0; i < 10; i += 1) {
+					$this.prepend('<div class="foo">' + i + '</div>');
+				}
+			});
+		},
+
+		appendContent = true,
+		updateContent = function () {
+
+			var $panel = $('.no5'),
+				$panelContent = $('.no5 > .sp-viewport > .sp-container'),
+				length = $panelContent.children().length;
+
+			if (length <= 0) {
+				appendContent = true;
+			} else if (length >= 10) {
+				appendContent = false;
+			}
+
+			if (appendContent) {
+				$panelContent.prepend('<div class="foo">' + length + '</div>');
+			} else {
+				$panelContent.children().eq(0).remove();
+			}
+
+			$panel.scrollpanel('update');
+		},
+
+		init = function () {
+
+			generateContent();
+			$('.scrollpanel').scrollpanel();
+
+			setInterval(updateContent, 1000);
+		};
+
+	$(init);
+
+}(jQuery));
